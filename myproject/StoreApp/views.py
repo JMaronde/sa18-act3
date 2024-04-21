319d305adf961eba58a05cc2c8290d2697c344f2
Jonathan Maronde <j2mike4@gmail.com>
2024-04-21 17:25:52 -0500

    Created the views and url patterns

--- myproject/StoreApp/views.py
+++ myproject/StoreApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
+
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/show.html', {'product': product})
