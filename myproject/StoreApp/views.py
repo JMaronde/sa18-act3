a1a5e31e6909679b545d6a27b55d6ca773b36d59
Jonathan Maronde <j2mike4@gmail.com>
2024-04-21 21:20:12 -0500

    Created the base html, and created the html for the index page. Corrected the links in the views.py to include the app name.

--- myproject/StoreApp/views.py
+++ myproject/StoreApp/views.py
@@ -4,8 +4,8 @@ from .models import Product
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'products/index.html', {'products': products})
+    return render(request, 'StoreApp/index.html', {'products': products})
 
 def product_details(request, id):
     product = get_object_or_404(Product, id=id)
-    return render(request, 'products/show.html', {'product': product})
+    return render(request, 'StoreApp/show.html', {'product': product})
